fab132ed746c19444515f99d3eb158f4a2c0cb63
bdirito91@gmail.com <bdirito91@gmail.com>
2018-06-19 17:02:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// write your code below!
+function HappyHolidays() {
+  console.log("Happy, holidays!");
+}
+function happyHolidaysTo() {
+  return `Happy holidays, ${name}!`;
+}
+function happyHolidaysTo() {
+  return `${holiday}, ${name}!`;
+}
+function holidayCountdown () {
+  return 
+}
